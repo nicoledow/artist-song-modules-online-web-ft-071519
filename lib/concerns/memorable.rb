dad8da9060ef46c0a63b804c69db2f0bc3939cf5
Nicole Dow <nikki.dow71@gmail.com>
2019-07-30 21:13:56 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,18 +3,15 @@ module Memorable
     def reset_all
       self.all.clear
     end
-  
-      def count
-        self.all.count
-      end
+
+    def count
+      self.all.count
+    end
   end
   
   module InstanceMethods
-    
-    def initalize
+    def initialize
       self.class.all << self
     end
-    
   end
-  
 end
